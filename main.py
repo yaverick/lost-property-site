c107c311b63ebb93a50c193e6219e31bc881f8ec
yaverick <yaroskush@gmail.com>
2026-04-19 22:40:53 +0300

    start

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
