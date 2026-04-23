ab07e47920e11c4dd31c50a783775990b2594f59
yaverick <yaroskush@gmail.com>
2026-04-24 00:36:27 +0300

    bd for users - begin

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///profiles.db'
+db = SQLAlchemy(app)
+
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
 
 @app.route('/')
 @app.route('/home')
@@ -15,8 +27,10 @@ def about():
 def login():
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
+    if request.method == 'POST':
+        print(request.form['username'])
     return render_template('register.html')
 
 @app.route('/rules')
