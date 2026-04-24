f288d42ad95a10d8f2f1b0076836532374efb37c
yaverick <yaroskush@gmail.com>
2026-04-25 01:05:33 +0300

    registration add, without log in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///profiles.db'
@@ -11,7 +12,7 @@ class Profile(db.Model):
     full_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     phone_number = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.Text, nullable=False)
 
 
 @app.route('/')
@@ -30,8 +31,41 @@ def login():
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
-        print(request.form['username'])
-    return render_template('register.html')
+        full_name = request.form['full-name']
+        email = request.form['email']
+        phone_number = request.form['phone']
+        password = (request.form['password']).rstrip()
+        confirm_password = (request.form['confirm-password']).rstrip()
+
+        hashed_password = generate_password_hash(password)
+
+        print(hashed_password)
+
+        new_profile = Profile(
+            full_name=full_name,
+            email=email,
+            phone_number=phone_number,
+            password=hashed_password)
+
+        error = False
+        if password == confirm_password:
+            try:
+                db.session.add(new_profile)
+                db.session.commit()
+                return redirect('/succesfuly-registered')
+            except:
+                return 'Error occurred while creating profile.'
+
+        else:
+            error = True
+            return render_template('register.html',
+                                   error=error,
+                                   full_name=full_name,
+                                   email=email,
+                                   phone_number=phone_number)
+
+    else:
+        return render_template('register.html')
 
 @app.route('/rules')
 def rules():
@@ -43,4 +77,6 @@ def report():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
