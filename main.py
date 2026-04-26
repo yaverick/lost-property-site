232b9f003ecf8ff34425c6e2518d0d2d1169a811
yaverick <yaroskush@gmail.com>
2026-04-26 19:32:08 +0300

    items db create without displaying

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from datetime import datetime
+import pytz
+
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///profiles.db'
+app.config['SQLALCHEMY_BINDS'] = {'lost_items': 'sqlite:///lost_items.db'}
 db = SQLAlchemy(app)
+
+
+def get_kyiv_time():
+    return datetime.now(pytz.timezone('Europe/Kyiv'))
+
+
+class Profile(db.Model, UserMixin):
+    __tablename__ = 'profile'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.Text, nullable=False)
+
+
+class LostItem(db.Model):
+    __bind_key__ = 'lost_items'
+    __tablename__ = 'lost_items'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.Text, nullable=False)
+    creator_id = db.Column(db.Integer, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=get_kyiv_time)
+
 
 app.secret_key = '5732857ukklqdl353shdgfs'
 
@@ -21,13 +51,6 @@ def load_user(user_id):
 @app.context_processor
 def inject_user():
     return dict(current_user=current_user)
-
-class Profile(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
-    phone_number = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.Text, nullable=False)
 
 
 @app.route('/')
@@ -72,8 +95,6 @@ def register():
 
         hashed_password = generate_password_hash(password)
 
-        print(hashed_password)
-
         new_profile = Profile(
             name=name,
             email=email,
@@ -87,7 +108,8 @@ def register():
                 db.session.commit()
                 return redirect('/login')
             except:
-                return 'Error occurred while creating profile.'
+                return render_template('error.html',
+                                       error='Під час свторення акаунту виникла помилка.')
 
         else:
             return render_template('register.html',
@@ -102,8 +124,31 @@ def register():
 def rules():
     return render_template('rules.html')
 
-@app.route('/report')
+@app.route('/report', methods=['POST', 'GET'])
+@login_required
 def report():
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+        city = request.form['city']
+        address = request.form['address']
+        creator_id = current_user.id
+
+        new_lost_item = LostItem(
+            title=title,
+            text=text,
+            city=city,
+            address=address,
+            creator_id=creator_id)
+
+        try:
+            db.session.add(new_lost_item)
+            db.session.commit()
+            return redirect('/home')
+        except:
+            return render_template('error.html',
+                                       error='Під час свторення знахідки виникла помилка.')
+
     return render_template('report.html')
 
 
