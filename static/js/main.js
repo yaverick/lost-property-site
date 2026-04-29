8d1f09175a6d616bb2b878b23d246e2c710be708
yaverick <yaroskush@gmail.com>
2026-04-29 17:49:45 +0300

    minor fixes

--- static/js/main.js
+++ static/js/main.js
@@ -3,12 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const navMenu = document.getElementById('nav-menu');
 
     hamburger.addEventListener('click', () => {
-        // Додаємо або забираємо клас 'active' при кліку
         hamburger.classList.toggle('active');
         navMenu.classList.toggle('active');
     });
 
-    // Опціонально: закривати меню при кліку на посилання
     document.querySelectorAll('.nav-links a').forEach(link => {
         link.addEventListener('click', () => {
             hamburger.classList.remove('active');
